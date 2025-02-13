8a211023981c4d17bdd6e32463886925dfb96649
Alex Sandro dos Santos <158177491+AlexSantos18@users.noreply.github.com>
2025-02-13 17:15:27 -0300

    Update CartasSuperTrunfo.c incluida densidade populacional e pib per capita
    Foram atualizado as informações de densidade populacional e pib per capita ,  incluído tbm as funções para uma melhor organização do código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+char estado[2];
+char cod_carta[5];
+char cidade[50];
+int populacao = 0;
+float area = 0;
+float pib = 0;
+int n_turisticos = 0;
+float dens_populacional = 0;
+float pib_capita = 0;
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+void imprimir_cab(void){
+    printf("=======================================\n");
+    printf("= Cadastramento da Carta Super Trunfo =\n");
+    printf("=======================================\n");
 
-    // Declaração das variaveis para utilizaçao no sistema
-    char estado[2];
-    char cod_carta[5];
-    char cidade[50];
-    int populacao = 0;
-    float area = 0;
-    float pib = 0;
-    int n_turisticos = 0;
-    
-    // Inicio da solicitaçao de dados dos usuarios
-    printf("Cadastramento da Carta Super Trunfo\n");
-    printf("==================================\n");
+}
+
+void imprimir_dados(void){
+    printf("=======================\n");
+    printf("=  Dados Cadastrados  =\n");
+    printf("=======================\n");
+}
+
+
+void entrada_dados(void){
     printf("Digite a letra correspondente ao Estado: ");
     scanf(" %s", estado);
     printf("Digite o Codigo da Carta: ");
@@ -39,28 +42,39 @@ int main() {
     printf("Carta Cadastrada com Sucesso.\n");
     printf("\n");
     printf("\n");
-   
+    getch();
 
-    // Impressao dos dados digitados pelo usuario
+}
 
-    printf("Dados da Carta Cadastrada\n");
-    printf("===========================\n");
+void saida_dados(void){
     printf("Estado: %s\n", estado);
     printf("Codigo da Carta: %s\n", cod_carta);
     printf("Nome da Cidade: %s\n", cidade);
     printf("Populacao: %d\n", populacao);
     printf("Area: %.2f km\n", area);
+    printf("Densidade Populacional: %.2f pessoas/km2\n", dens_populacional);
     printf("PIB: %.2f bilhoes de reais\n", pib);
+    printf("PIB per Capita: %.2f reais\n", pib_capita);
     printf("Numero de Pontos Turisticos: %d\n", n_turisticos);
-    printf("===========================\n");
+    printf("\n");
+    printf("\n");
+    getch();
 
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+}
+
+int main() {
     
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Inicio da solicitaçao de dados dos usuarios
+    imprimir_cab();
+    entrada_dados();
+
+    // Calculo da Densidade e Per Capita
+    dens_populacional = (float)(populacao / area);
+    pib_capita = (float)(pib/populacao);
+    
+    // Impressao dos dados digitados pelo usuario
+    imprimir_dados();
+    saida_dados();
 
     return 0;
 }
